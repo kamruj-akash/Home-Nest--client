d43852334bd0f9c9782a42c5ad69614313ca6a48
kamruj-akash <source.akash@gmail.com>
2025-11-12 13:12:06 +0600

    replace vercel API on axios baseURL

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const useAxios = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "https://akash-home-nest-server.vercel.app",
   });
   return instance;
 };
